c4c9ccb7191fa16602ff9e8bba37496070688081
januardlg <januargaol48@gmail.com>
2025-09-17 10:59:28 +0700

    add common error handler

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,25 @@
-// import express from 'express';
-// // // import itemRoutes from './routes/itemRoutes';
-// // // import { errorHandler } from './middlewares/errorHandler';
-// import indexRoutes from './routes/index.js'
+import express from 'express';
 
-// const app = express();
+import { unexpectedErrorHandler } from './middlewares/unexpected-error-handler.js';
 
-// app.use(express.json());
-
-// // Routes
-// // app.use('/api/items', itemRoutes);
-// app.use('/', indexRoutes)
-
-// // Global error handler (should be after routes)
-// // app.use(errorHandler);
-
-// export default app;
-
-import express from "express";
+// import routes
+import indexRoutes from './routes/index.js'
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello from app.ts using nodemon.  tes");
+app.use(express.json());
+
+// Routes
+// insert to app
+app.use('/', indexRoutes)
+
+// Global error handler (should be after routes)
+// app.use(errorHandler);
+
+app.use(function (req, res, next) {
+    res.status(404).json({ success: false, message: "Route not found" });
 });
+
+app.use(unexpectedErrorHandler);
 
 export default app;
